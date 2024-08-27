fc84c22659533ecd2cacb20ba6e319a004b14c79
phani-2429 <ushasreevalluri041978@gmail.com>
2024-08-27 15:51:24 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,49 @@
+function firstplace(){
+    document.querySelector("#button1").href="/tajmahal.html";
+    a= new SpeechSynthesisUtterance("first place is Taj Mahal, Agra These place is located in Uttar Pradesh  It's Popular for: The Taj Mahal is an architectural marvel and a UNESCO World Heritage Site, known globally as a symbol of love. Millions visit annually to witness its stunning white marble structure")
+    speechSynthesis.speak(a)
+}
+function secondplace(){
+ document.querySelector("#secondplace").href="/secondplace.html";
+ b= new SpeechSynthesisUtterance("2. Jaipur, Rajasthan  It's Popular for: The pink city is renowned for its majestic forts, palaces, and vibrant culture. Amber Fort, City Palace, and Hawa Mahal are must-see attractions, making Jaipur a key destination on the Golden Triangle tourist circuit.")
+ speechSynthesis.speak(b)
+}
+function thirdplace(){
+     document.querySelector("#thirdplace").href="/thirdplace.html"
+     c= new SpeechSynthesisUtterance("thirdplace Vijayawada, Andhra Pradesh It's Popular for: Vijayawada is an important cultural and commercial hub in Andhra Pradesh. Key attractions include the Kanaka Durga Temple, Prakasam Barrage, and the Undavalli Caves. The city is also known for its vibrant festivals and rich history")
+     speechSynthesis.speak(c)
+}
+function fourthplace(){
+     document.querySelector("#fourthplace").href="/fourthplace.html"
+     d= new SpeechSynthesisUtterance("fourthplace Goa It's Popular for: Goa is famous for its beautiful beaches, vibrant nightlife, and rich Portuguese heritage. It offers a perfect blend of relaxation and adventure, making it a favorite among both domestic and international tourists.")
+     speechSynthesis.speak(d)
+}
+function fifthplace(){
+     document.querySelector("#fifthplace").href="/fifthplace.html"
+     e= new SpeechSynthesisUtterance("fifthplace Kerala Backwaters It was located in: Kerala  It's Popular for: The serene backwaters, with their interconnected lagoons and canals, offer a unique and peaceful experience of Kerala’s natural beauty. A houseboat cruise here is an unforgettable experience.")
+     speechSynthesis.speak(e)
+}
+function sixthplace(){
+     document.querySelector("#sixthplace").href="/sixthplace.html";
+     f= new SpeechSynthesisUtterance("sixthplace Mysore, Karnataka  It's Popular for : Mysore is famous for its royal heritage, particularly the grand Mysore Palace, which is illuminated with thousands of lights during festivals. The city is also known for its silk, sandalwood, and the Dussehra festival.")
+     speechSynthesis.speak(f)
+}
+function seventhplace(){
+     document.querySelector("#seventhplace").href="/seventhplace.html"
+     g= new SpeechSynthesisUtterance(" seventhplace Amritsar, Punjab  It's Popular for: Amritsar is home to the Golden Temple, the holiest gurdwara of Sikhism, which attracts millions of pilgrims and tourists. The city is also known for the Wagah Border ceremony and its rich Punjabi culture and cuisine.")
+     speechSynthesis.speak(g)
+}
+function eightplace(){
+     document.querySelector("#eightplace").href="/eightplace.html"
+     h= new SpeechSynthesisUtterance("eightplace Varanasi, Uttar Pradesh  It's Popular for: As one of the oldest living cities in the world and the spiritual heart of India, Varanasi attracts pilgrims and tourists alike. The Ganga Aarti on the banks of the Ganges River is a deeply spiritual and moving experience")
+     speechSynthesis.speak(h)
+}
+function ninethplace(){
+     document.querySelector("#ninethplace").href="/ninethplace.html"
+     i= new SpeechSynthesisUtterance("nineth place Leh-Ladakh, Jammu & Kashmir  It's Popular for : Leh-Ladakh offers breathtaking landscapes, including high-altitude deserts, pristine lakes, and towering mountains. It’s a paradise for adventure enthusiasts and those seeking spiritual peace in the Himalayas")
+     speechSynthesis.speak(i)
+}
+function back(){
+     document.querySelector("#back").href="C:/Users/Admin/project%201%20popular%20places%20in%20india/index.html";
+     speechSynthesis.cancel()
+}
